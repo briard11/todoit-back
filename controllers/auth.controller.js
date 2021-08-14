c770c857779b9b5c4b5c8d3e50dd1192f3ff17cd
david antonio barrios <briard1996@gmail.com>
2021-08-14 14:42:06 -0500

    Google Sign Added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const bcryptjs = require("bcryptjs");
 const Usuario = require("../models/usuario.model");
 const { response, request } = require("express");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req = request, res = response) => {
   const { correo, password } = req.body;
@@ -45,4 +46,40 @@ const login = async (req = request, res = response) => {
   }
 };
 
-module.exports = { login };
+const googleSignin = async (req, res = response) => {
+  try {
+    const { id_token } = req.body;
+    const { correo, nombre, img } = await googleVerify(id_token);
+    let usuario = await Usuario.findOne({ correo });
+    
+    if (!usuario) {
+      // Creacion
+      const data = {
+        nombre,
+        correo,
+        password: ":P",
+        img,
+        google: true,
+      };
+      
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+    
+    if (!usuario.estado) {
+      res.status(401).json({ msg: "Hable con el admin, usuario bloqueado" });
+    }
+
+    // Generar el JWT
+    const token = await generarJWT(usuario._id);
+
+    res.json({
+      msg: "todo ok",
+      token
+    });
+  } catch (error) {
+    res.status(400).json({ msg: "Token de Google no es valido" });
+  }
+};
+
+module.exports = { login, googleSignin };
