f0d6fabefcd4b487d776e71c6154289ed6db8a01
david antonio barrios <briard1996@gmail.com>
2021-11-02 17:33:51 -0500

    add dev script to start app, modify header authorization with bearer

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -4,22 +4,28 @@ const jwt = require("jsonwebtoken");
 const Usuario = require("../models/usuario.model");
 
 const validarJWT = async (req = request, res = response, next) => {
-  const token = req.header("x-token");
-  if (!token) {
+  const auth = req.header("Authorization");
+  if (!auth) {
     return res.status(401).json({
       msg: "No hay token en la peticion",
     });
   }
 
   try {
+    const token = auth.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        msg: "No hay token en la peticion",
+      });
+    }
     const { uid } = jwt.verify(token, process.env.SECRETORPRIVETEKEY);
     // Leer el usuario que corresponde al uid
     usuario = await Usuario.findById(uid);
 
-    if(!usuario){
-        return res.status(401).json({
-            msg: "Token no valido",
-          });
+    if (!usuario) {
+      return res.status(401).json({
+        msg: "Token no valido",
+      });
     }
 
     // Verificar si el uid no esta marcado como inactivo
